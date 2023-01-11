0e40b42b5386320e6ed0821b06a1c377b3e27cc1
Naszh <naszh99@gmail.com>
2023-01-11 22:08:35 +0300

    add informational text to projects

--- src/components/main/blockPortfolio/portfolio.styled.ts
+++ src/components/main/blockPortfolio/portfolio.styled.ts
@@ -3,51 +3,55 @@ import { VscGithub } from 'react-icons/vsc';
 import { SiVercel } from 'react-icons/si';
 import { BlockContainer, ItemWithBorder } from '../../common/common.styles';
 
+export const Notification = styled.p`
+  text-align: justify;
+`;
+
 export const ContainerPortf = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	gap: 15px;
-	@media (max-width: 1024px) {
-		grid-template-columns: 1fr;
-	}
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 15px;
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const ProjectBox = styled(BlockContainer)`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Header = styled.h4`
-	text-align: center;
-	font-size: 1.1rem;
-	margin: 0;
+  text-align: center;
+  font-size: 1.1rem;
+  margin: 0;
 `;
 
 export const PreviewImg = styled.img`
-	width: 100%;
+  width: 100%;
 `;
 
 export const GithubButton = styled(VscGithub)`
-	font-size: 2rem;
-	vertical-align: middle;
-	margin: 5px;
-	&:hover {
-		transform: scale(1.05);
-	}
+  font-size: 2rem;
+  vertical-align: middle;
+  margin: 5px;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 export const VercelButton = styled(SiVercel)`
-	font-size: 2rem;
-	vertical-align: middle;
-	margin: 5px;
-	&:hover {
-		transform: scale(1.3);
-	}
+  font-size: 2rem;
+  vertical-align: middle;
+  margin: 5px;
+  &:hover {
+    transform: scale(1.3);
+  }
 `;
 
 export const SmallerItemWithBorder = styled(ItemWithBorder)`
-	font-size: 0.9rem;
-	border-style: dotted;
-	padding: 4px;
-	margin: 2px;
+  font-size: 0.9rem;
+  border-style: dotted;
+  padding: 4px;
+  margin: 2px;
 `;
--- src/components/main/blockPortfolio/portfolio.tsx
+++ src/components/main/blockPortfolio/portfolio.tsx
@@ -4,66 +4,71 @@ import { ProjectsList } from '../../../constants/data';
 import { ThemeContext } from '../../../theme/themeProvider';
 
 import {
-	BlockContainer,
-	BlockHeader,
-	BlockHeaderSpan,
-	ItemsList,
-	DefaultLink,
-	Text,
+  BlockContainer,
+  BlockHeader,
+  BlockHeaderSpan,
+  ItemsList,
+  DefaultLink,
+  Text,
 } from '../../common/common.styles';
 import {
-	ContainerPortf,
-	GithubButton,
-	Header,
-	ProjectBox,
-	VercelButton,
-	PreviewImg,
-	SmallerItemWithBorder,
+  Notification,
+  ContainerPortf,
+  GithubButton,
+  Header,
+  ProjectBox,
+  VercelButton,
+  PreviewImg,
+  SmallerItemWithBorder,
 } from './portfolio.styled';
 
 export const MyPortfolio = () => {
-	const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
-	return (
-		<BlockContainer>
-			<BlockHeaderSpan theme={theme}>What did I do</BlockHeaderSpan>
-			<BlockHeader>Recent Projects</BlockHeader>
-			<ContainerPortf>
-				{ProjectsList.map((project, i) => {
-					return (
-						<ProjectBox key={i}>
-							<Header>
-								{project.title}
-								<DefaultLink href={project.githubLink} target='_blank'>
-									<GithubButton
-										style={{
-											color: theme === 'light' ? '#034640' : '#D1CCB9',
-										}}
-									/>
-								</DefaultLink>
-								{project.deployLink && (
-									<DefaultLink href={project.deployLink} target='_blank'>
-										<VercelButton
-											style={{
-												color: theme === 'light' ? '#034640' : '#D1CCB9',
-											}}
-										/>
-									</DefaultLink>
-								)}
-							</Header>
-							<PreviewImg src={project.imgSrc} alt='Project preview' />
-							<Text>{project.description}</Text>
-							<ItemsList>
-								{project.stack.map((el, i) => (
-									<SmallerItemWithBorder key={i} theme={theme}>
-										{el}
-									</SmallerItemWithBorder>
-								))}
-							</ItemsList>
-						</ProjectBox>
-					);
-				})}
-			</ContainerPortf>
-		</BlockContainer>
-	);
+  return (
+    <BlockContainer>
+      <BlockHeaderSpan theme={theme}>What did I do</BlockHeaderSpan>
+      <BlockHeader>Recent Projects</BlockHeader>
+      <Notification>
+        Soon there will be an instagram project for cats ฅ^•ﻌ•^ฅ which I am
+        developing using javascript, typescript, react, redux and backend part.
+      </Notification>
+      <ContainerPortf>
+        {ProjectsList.map((project, i) => {
+          return (
+            <ProjectBox key={i}>
+              <Header>
+                {project.title}
+                <DefaultLink href={project.githubLink} target='_blank'>
+                  <GithubButton
+                    style={{
+                      color: theme === 'light' ? '#034640' : '#D1CCB9',
+                    }}
+                  />
+                </DefaultLink>
+                {project.deployLink && (
+                  <DefaultLink href={project.deployLink} target='_blank'>
+                    <VercelButton
+                      style={{
+                        color: theme === 'light' ? '#034640' : '#D1CCB9',
+                      }}
+                    />
+                  </DefaultLink>
+                )}
+              </Header>
+              <PreviewImg src={project.imgSrc} alt='Project preview' />
+              <Text>{project.description}</Text>
+              <ItemsList>
+                {project.stack.map((el, i) => (
+                  <SmallerItemWithBorder key={i} theme={theme}>
+                    {el}
+                  </SmallerItemWithBorder>
+                ))}
+              </ItemsList>
+            </ProjectBox>
+          );
+        })}
+      </ContainerPortf>
+    </BlockContainer>
+  );
 };
